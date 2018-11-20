cb7c210cb8baca27a98dfd25fe09fe8817529ab5
FTadros <43042263+FTadros@users.noreply.github.com>
2018-11-20 12:56:04 -0500

    Update Gameover.py

--- Gameover.py
+++ Gameover.py
@@ -20,3 +20,28 @@ def draw():
         textSize(200)
         text("Game over",  650, 250)
         
+
+        
+        
+   #flying
+circley = 450
+
+
+def setup():
+    size(1300,500)
+    
+    
+def draw():
+    global circley
+    background(255)
+    ellipse(50,circley,100,100)
+    
+    if key == 'w' and circley >= 50:
+        circley -= 2
+    
+    elif circley <= 5:
+        circley = 450
+    
+    elif key == 's' and circley <= 450:
+        circley += 2
+    print circley
